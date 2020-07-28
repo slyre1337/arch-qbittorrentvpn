c43c708c44739b4bfc8aa5f7380570dc75813e26
slyre1337 <59583684+slyre1337@users.noreply.github.com>
2020-07-28 17:57:54 +0200

    Update qbittorrent.sh

--- run/nobody/qbittorrent.sh
+++ run/nobody/qbittorrent.sh
@@ -64,6 +64,19 @@ if [[ "${VPN_PROV}" == "pia" && -n "${VPN_INCOMING_PORT}" ]]; then
 	fi
 
 	# note -k flag required to support insecure connection (self signed certs) when https used
+	
+	IPv4_ADDR=${vpn_ip}
+	IFS='.' read -ra ADDR <<< "$IPv4_ADDR"
+	function d2b() {
+		printf "%08d" $(echo "obase=2;$1"|bc)
+	}
+	port_bin="$(d2b ${ADDR[2]})$(d2b ${ADDR[3]})"
+	port_dec=$(printf "%04d" $(echo "ibase=2;${port_bin:4}"|bc))
+	for i in 1 2 3; do
+	echo "$i$port_dec
+	VPN_INCOMING_PORT=$i$port_dec
+	echo XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX SET FOR TESTING "$VPN_INCOMING_PORT"
+	
 	curl -k -i -X POST -d "json={\"random_port\": false}" "${web_protocol}://localhost:${WEBUI_PORT}/api/v2/app/setPreferences" &> /dev/null
 	curl -k -i -X POST -d "json={\"listen_port\": ${VPN_INCOMING_PORT}}" "${web_protocol}://localhost:${WEBUI_PORT}/api/v2/app/setPreferences" &> /dev/null
 
